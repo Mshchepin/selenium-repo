897ea2fc97691a370c6687e112684c6b475f11d1
Mshchepin <140291948+Mshchepin@users.noreply.github.com>
2023-07-24 21:46:04 +0700

    Отредактированная версия теста

--- main.py
+++ main.py
@@ -7,29 +7,32 @@ from selenium.webdriver.common.by import By
 import selenium
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-wd = webdriver.Chrome()
-#@pytest.fixture
-#def driver(request):
-  #  wd = webdriver.Chrome()
-   # request.addfinalizer(wd.quit)
-   # return wd
+#wd = webdriver.Chrome()
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
 
-#def test_example(driver):
-    #webdriver.get('https://www.google.ru/?hl=ru')
-    #webdriver.find_element_by_name('q').send_keys('webdriver')
-   # webdriver.find_element_by_name('BtnG').click()
-   # WebDriverWait(webdriver, 10).until(EC.title_is("Webdriver - Поиск в Google"))
+def test_example(driver):
+    driver.get('https://www.google.ru/?hl=ru')
+    driver.find_element(By.NAME, "q").send_keys('webdriver')
+    button = driver.find_element(By.NAME, "btnK")
+    time.sleep(3)
+    button.click()
+    time.sleep(3)
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
 
 
 if __name__ == '__main__':
     print('PyCharm1')
 
-    wd.get("https://www.google.com/")
-    q = wd.find_element(By.NAME, "q")
-    q.send_keys("webdriver")
-    time.sleep(3)
-    b = wd.find_element(By.NAME, "btnK")
-    b.is_displayed()
-    b.click()
-    #time.sleep(5)
-    WebDriverWait(wd, 10).until(EC.title_is("webdriver - Поиск в Google"))
+#     wd.get("https://www.google.com/")
+#     q = wd.find_element(By.NAME, "q")
+#     q.send_keys("webdriver")
+#     time.sleep(3)
+#     b = wd.find_element(By.NAME, "btnK")
+#     b.is_displayed()
+#     b.click()
+#     #time.sleep(5)
+#     WebDriverWait(wd, 10).until(EC.title_is("webdriver - Поиск в Google"))
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,19 +1,24 @@
+import time
+
 import pytest
-import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import selenium
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
+#wd = webdriver.Chrome()
 @pytest.fixture
 def driver(request):
-    wd = webdriver.chrome()
+    wd = webdriver.Chrome()
     request.addfinalizer(wd.quit)
     return wd
 
-
 def test_example(driver):
     driver.get('https://www.google.ru/?hl=ru')
-    driver.find_element_by_name('q').send_keys('webdriver')
-    driver.find_element_by_name('BtnG').click()
-    WebDriverWait(driver, 10).until(EC.title_is("Webdriver - Поиск в Google"))
+    driver.find_element(By.NAME, "q").send_keys('webdriver')
+    button = driver.find_element(By.NAME, "btnK").click()
+    time.sleep(3)
+    #button.click()
+    time.sleep(3)
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
