9496c765699c7b42212d7fc8c4dbfae3337b99cb
Mshchepin <140291948+Mshchepin@users.noreply.github.com>
2023-10-09 22:45:10 +0700

    13 задание.

--- tests/test_13_Bucket.py
+++ tests/test_13_Bucket.py
@@ -28,7 +28,6 @@ def test_login(driver):
     wait = WebDriverWait(driver, 10)
     driver.get('http://localhost/litecart/public_html/en/')
 
-
     for i in range(1, 4):
         Product = driver.find_element(By.CSS_SELECTOR, "#box-most-popular > div > ul > li:nth-child(1)")
         Product.click()
@@ -45,13 +44,24 @@ def test_login(driver):
             Small.click()
             print("Есть Size")
 
-
         AddToCartButton = driver.find_element(By.NAME, "add_cart_product")
         AddToCartButton.click()
-        time.sleep(5)
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#cart > a.content > span.quantity"), "1"))
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#cart > a.content > span.quantity"), str(i)))
         print(driver.find_element(By.CSS_SELECTOR, "#cart > a.content > span.quantity"))
-
+        HomeButton = driver.find_element(By.CSS_SELECTOR, "#site-menu > ul > li.general-0 > a > i")
+        HomeButton.click()
+    CheckoutButton = driver.find_element(By.CSS_SELECTOR, "#cart > a.link")
+    CheckoutButton.click()
+    wait.until(EC.element_to_be_clickable((By.NAME, "remove_cart_item")))
+    time.sleep(2)
+    while len(driver.find_elements(By.CSS_SELECTOR, "#order_confirmation-wrapper > table > tbody > tr:nth-child(2)")) > 0:
+        wait.until(EC.element_to_be_clickable((By.NAME, "remove_cart_item")))
+        RemoveButton = driver.find_element(By.NAME, "remove_cart_item")
+        RemoveButton.click()
+        print("Deleted")
+        time.sleep(2)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#checkout-cart-wrapper > p:nth-child(1) > em")))
+    #assert len(driver.find_elements(By.CSS_SELECTOR, "#order_confirmation-wrapper > table > tbody > tr:nth-child(2)")) == 0
 
 
 
